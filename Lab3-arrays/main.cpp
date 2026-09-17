c1d0d27d3b9a2790470be808f8e320a02dae8573
Jesus <jesus.martinez.eece@gmail.com>
2026-09-17 01:56:39 -0500

    cs2

---
+++ Lab3-arrays/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+// return the sum of all elements in ar
+int sumArray(const int ar[], int size)
+{
+
+}
+
+// multiply all elements in ar and return the product
+long productArray(const int ar[], int size)
+{
+
+}
+
+// return the maximum element in ar
+int maxArray(const int ar[], int size)
+{
+
+}
+
+// return the minimum element in ar
+int minArray(const int ar[], int size)
+{
+
+}
+
+int main()
+{
+    /*
+    Q1: declare an integer array and initialize it with the first 10 prime numbers. You can manually initialize the values.
+    */
+
+
+
+
+
+
+
+    /*
+    Q2. Complete the function definitions and test them using the following function calls.
+    */
+   int array2[6] = {10, 20, 30, 6, 50, -100};
+   cout << "Sum of elements: " << sumArray(array2, 6) << endl;
+   cout << "Product of elements: " << productArray(array2, 6) << endl;
+   cout << "Maximum element: " << maxArray(array2, 6) << endl;
+   cout << "Minimum element: " << minArray(array2, 6) << endl;
+
+   /*
+   Q3. Declare three floating-point arrays of size 4 x 4. Ask user to input values in the first array, and perform element-wise addition of first two arrays and store the result in third array. Print the third array.
+
+   Example of element-wise addition (this is also known as matrix addition)
+   10 45 20 -3                55 23 17 21                  65 68 37 18
+   60 56 -6 81                -4 31 18 16                  56 87 12 97
+   -2 -4 45 60     +          15 20 25 30        =         13 16 70 90
+   22 14 16 38                -6 18 10 -9                  16 32 26 29
+   */
+
+
+}
